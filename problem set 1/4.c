dd13618546ef2a6c04241db3a23aa78bfd4735c1
1915002506 <72783875+1915002506@users.noreply.github.com>
2020-10-19 17:52:18 +0600

    Add files via upload

---
+++ problem set 1/4.c
@@ -0,0 +1,17 @@
+/*
+problem Name:print entire array each element in newline.
+Name:Supriya Debnath
+ID:1915002506
+
+*/
+#include<stdio.h>
+int main(){
+    int a[100] = {1,2,3,4,5,6,7,8,9,10},max_size= 100, size= 10;
+    int i;
+    for(i=0;i<size;i++)
+    {
+        printf("%d\n", a[i]);
+    }
+
+  return 0;
+}
